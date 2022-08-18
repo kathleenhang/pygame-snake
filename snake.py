b5466e094986c5b6736f42c77d53c76681e0a86c
kathleenhang <kathleenhang@live.com>
2022-08-18 13:02:39 -0700

    add class main

--- snake.py
+++ snake.py
@@ -4,11 +4,31 @@ import sys
 from pygame.math import Vector2
 
 
+class Main:
+    def __init__(self):
+        self.snake = Snake()
+        self.food = Food()
+
+    def draw_elements(self):
+        self.check_collision()
+        self.snake.draw_snake()
+        self.food.draw_food()
+
+    def update(self):
+        self.snake.move_snake()
+
+    def check_collision(self):
+        if self.snake.body[0] == self.food.pos:
+            self.snake.will_grow = True
+            self.food.move_food()
+
+
 class Snake:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
         # move right
         self.direction = Vector2(1, 0)
+        self.will_grow = False
 
     def draw_snake(self):
         for block in self.body:
@@ -20,20 +40,27 @@ class Snake:
             pygame.draw.rect(screen, green_color, snake_rect)
 
     def move_snake(self):
-        # list with last item removed
-        body_copy = self.body[:-1]
-        self.body = body_copy
-        # create new head based off of direction that the user inputted
-        new_head = self.direction + self.body[0]
-        self.body.insert(0, new_head)
+        if not self.will_grow:
+            # list with last item removed
+            body_copy = self.body[:-1]
+            self.body = body_copy
+            # create new head based off of direction that the user inputted
+            new_head = self.direction + self.body[0]
+            self.body.insert(0, new_head)
+        else:
+            # list with last item removed
+            body_copy = self.body[:]
+            self.body = body_copy
+            # create new head based off of direction that the user inputted
+            new_head = self.direction + self.body[0]
+            self.body.insert(0, new_head)
+            self.will_grow = False
 
 
 class Food:
     def __init__(self):
         # randomize food location
-        self.x = random.randint(0, cell_count-1)
-        self.y = random.randint(0, cell_count-1)
-        self.pos = Vector2(self.x, self.y)
+        self.move_food()
 
     def draw_food(self):
         # create food rectangle
@@ -41,6 +68,11 @@ class Food:
                                 self.pos.y * cell_size, cell_size, cell_size)
         white_color = (255, 255, 255)
         pygame.draw.rect(screen, white_color, food_rect)
+
+    def move_food(self):
+        self.x = random.randint(0, cell_count-1)
+        self.y = random.randint(0, cell_count-1)
+        self.pos = Vector2(self.x, self.y)
 
 
 pygame.init()
@@ -50,19 +82,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 black_color = pygame.color.Color('#000000')
 
 
-x = screen_width/3
-y = screen_height/3
-
-# surface
-blue_surface = pygame.Surface((30, 30))
-blue_surface.fill((0, 0, 255))
-# position blue rect center at screen midpoint
-blue_rect = blue_surface.get_rect(center=(screen_width/2, screen_height/2))
-
-
 # track time
 clock = pygame.time.Clock()
-
 
 # create grid
 
@@ -70,10 +91,8 @@ clock = pygame.time.Clock()
 cell_size = 30
 # 20x20 grid
 cell_count = 20
+main = Main()
 
-
-food = Food()
-snake = Snake()
 # create a custom event for screen updates
 screen_update = pygame.USEREVENT
 # custom screen update event triggers every 150 ms
@@ -82,23 +101,20 @@ while 1:
 
     for event in pygame.event.get():
         if event.type == screen_update:
-            snake.move_snake()
+            main.snake.move_snake()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:  # right
-                snake.direction = Vector2(1, 0)
+                main.snake.direction = Vector2(1, 0)
             if event.key == pygame.K_a:  # left
-                snake.direction = Vector2(-1, 0)
+                main.snake.direction = Vector2(-1, 0)
             if event.key == pygame.K_w:  # up
-                snake.direction = Vector2(0, -1)
+                main.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_s:  # down
-                snake.direction = Vector2(0, 1)
-        # fill bg color before snake/food
+                main.snake.direction = Vector2(0, 1)
+        # draw elements: board, snake, food
         screen.fill(black_color)
-        food.draw_food()
-        snake.draw_snake()
+        main.draw_elements()
 
-        # draw the elements
-        screen.blit(blue_surface, blue_rect)
         # quit game
         if event.type == pygame.QUIT:
             sys.exit()
